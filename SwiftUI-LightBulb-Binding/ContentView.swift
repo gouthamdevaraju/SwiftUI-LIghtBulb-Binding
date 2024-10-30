6ed8af35f4f25bf959b92202efcb85654591fb31
Goutham <gouthamdevaraju@gmail.com>
2024-10-30 16:03:16 +0530

    Added code for bulb view.

--- SwiftUI-LightBulb-Binding/ContentView.swift
+++ SwiftUI-LightBulb-Binding/ContentView.swift
@@ -7,15 +7,34 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct LightBulb: View {
+    
+    @Binding var turnOn: Bool
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: "lightbulb")
+                .font(.largeTitle)
+                .foregroundStyle(turnOn ? .red : .white)
+            Button("Toggle") {
+                turnOn.toggle()
+            }
         }
         .padding()
+    }
+}
+
+struct ContentView: View {
+    
+    @State private var turnOnRef: Bool = false
+    
+    var body: some View {
+        VStack {
+            LightBulb(turnOn: $turnOnRef)
+        }
+        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(turnOnRef ? .yellow : .black)
     }
 }
 
