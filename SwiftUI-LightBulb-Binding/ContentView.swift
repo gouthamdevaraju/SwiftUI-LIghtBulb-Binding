f0bd550c64ae022a8d1eea92c30a1710dcd18ef0
Goutham <gouthamdevaraju@gmail.com>
2024-10-30 15:54:22 +0530

    Added new project to the repo

---
+++ SwiftUI-LightBulb-Binding/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-LightBulb-Binding
+//
+//  Created by Goutham Devaraju on 30/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-LightBulb-Binding/SwiftUI_LightBulb_BindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_LightBulb_BindingApp.swift
+//  SwiftUI-LightBulb-Binding
+//
+//  Created by Goutham Devaraju on 30/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_LightBulb_BindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
